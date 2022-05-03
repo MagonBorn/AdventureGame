5eea16d69bc5f55d32817b95944b2d15b5678485
Matthew Mason <m.mason29272@gmail.com>
2022-05-03 16:03:21 +0100

    Added TreeNode Class

--- Game.py
+++ Game.py
@@ -1,2 +1,8 @@
+# ---------- Classes ---------- #
+class TreeNode:
+    def __init__(self, story_piece):
+        self.story_piece = story_piece
+        self.choices = []
+
 # ---------- Testing Area ---------- #
 print("Once Upon A Time")
